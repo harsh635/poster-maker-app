2a113302a7ce528a181a79e84c33ec5a89b6e629
harsh635 <78408802+harsh635@users.noreply.github.com>
2025-07-27 21:38:38 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,12 @@ import hashlib
 from starlette.responses import Response
 from starlette.staticfiles import StaticFiles
 from typing import Optional
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
 # ---------------------- LOAD ENV ------------------------
 load_dotenv()
 
